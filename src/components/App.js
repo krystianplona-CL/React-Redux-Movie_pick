9082d35d838cc15781b6a1a650f89ce9c6ba4fbd
Krystian Plona <krystianplona@gmail.com>
2018-01-13 11:06:47 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import '../style/App.css';
+
+import MainPage from './MainPage'
+import MovieList from './MovieList'
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <Route path="/" exact={true} render={() => {
+            return <MainPage/>
+          }}/>
+          <Route path="/movie_list" exact={true} render={() => {
+            return <MovieList/>
+          }}/>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/MovieList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../style/App.css';
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div>
+        MovieList
+      </div>
+    );
+  }
+}
+
+export default MainPage;
