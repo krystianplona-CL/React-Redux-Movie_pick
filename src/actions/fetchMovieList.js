16b884fcf13ad41f962f9c7ac00d61ab7caa88db
Krystian Plona <krystianplona@gmail.com>
2018-01-20 12:06:00 +0100

    post movie in progress

--- src/actions/fetchMovieList.js
+++ src/actions/fetchMovieList.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '';
+const API_KEY = 'AIzaSyBBgz_WQRMniH2WaAYoOSDi2sbFDMxs8jo';
 const ROOT_URL = `https://movie-picker-ff027.firebaseio.com/.json`;
 
 export const FETCH_MOVIE_LIST = 'FETCH_MOVIE_LIST';
--- src/containers/MoviesList.js
+++ src/containers/MoviesList.js
@@ -1,19 +1,60 @@
 import React,{ Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 class MoviesList extends Component {
+  constructor(props){
+    super(props);
 
+    this.renderMovie = this.renderMovie.bind(this);
+  }
+
+  onButtonClick(movieData) {
+
+    const ROOT_URL = `https://movie-picker-ff027.firebaseio.com/`;
+    console.log(movieData);
+    axios({
+      method: 'get',
+      url: 'https://movie-picker-ff027.firebaseio.com/.json',
+    }).then((data)=>{
+      const number = data.data.length ++;
+      axios({
+        method: 'post',
+        url: `https://movie-picker-ff027.firebaseio.com/${number}/.json`,
+        data: {
+          Title: movieData.Title,
+          Released: movieData.Released,
+          Runtime: movieData.Runtime,
+        }
+      })
+    });
+
+
+
+  }
   renderMovie(element) {
     if(element.Response === "False"){
       return null
     }
-    return (
-      <tr key={element.Title}>
-        <td>{element.Title}</td>
-        <td>{element.Released}</td>
-        <td>{element.Runtime}</td>
-      </tr>
-    )
+    else{
+      const movieData = {
+        Title: element.Title,
+        Released: element.Released,
+        Runtime: element.Runtime
+      }
+      return (
+        <tr key={element.Title}>
+          <td>{element.Title}</td>
+          <td>{element.Released}</td>
+          <td>{element.Runtime}</td>
+          <td>
+            <button type="button" className="btn btn-success" onClick={()=>{this.onButtonClick(movieData)}}>
+              Add
+            </button>
+          </td>
+        </tr>
+      )
+    }
   }
 
   render() {
@@ -24,6 +65,7 @@ class MoviesList extends Component {
             <th>Title</th>
             <th>Released</th>
             <th>Runtime</th>
+            <th>Add</th>
           </tr>
         </thead>
         <tbody>
